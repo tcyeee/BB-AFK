a9bdf5bd0b3a8d9cb15606ce615b51b93de4a580
tcyeee <tcyeee@outlook.com>
2021-12-24 22:22:25 +0800

    [develop] 逻辑代码更新

--- src/main/java/top/tcyeee/listener/PlayListener.java
+++ src/main/java/top/tcyeee/listener/PlayListener.java
@@ -5,33 +5,43 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerDropItemEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.event.player.PlayerSwapHandItemsEvent;
 import top.tcyeee.utils.EhCacheUtil;
 
 /**
+ * 玩家进入服务器/移动 -> 设置为活跃状态(5分钟过期)
+ * <p>
+ * 定时任务:
+ * 每间隔1分钟查询一次缓存:
+ * 1.比对playerlist和缓存的差值,其中没有的,存入挂机缓存{user,setTime},表示为挂机状态
+ * 2.查询挂机缓存, 挂机时间与当前时间差值为 n 的倍数, 则给与经验x,金币x
+ * 3.退出服务器时候,删除挂机缓存
+ *
  * @author tcyeee
  * @date 2021/12/23 16:36
  */
 public class PlayListener implements Listener {
-    // 玩家切换左右手事件
+    // 玩家移动事件
     @EventHandler
-    public void event(PlayerSwapHandItemsEvent event) {
+    public void event(PlayerMoveEvent event) {
         EhCacheUtil.setActive(event.getPlayer());
-        event.getPlayer().sendMessage(ChatColor.YELLOW + "切换为活跃状态");
     }
-    // 玩家移动 => ehcache存值 {user,5分钟过期}
-    // 定时任务
-    // 每间隔1分钟查询一次缓存:
-    //       1.比对playerlist和缓存的差值,其中没有的,存入挂机缓存{user,setTime},表示为挂机状态
-    //       2.查询挂机缓存, 挂机时间与当前时间差值为 n 的倍数, 则给与经验x,金币x
-    //       3.退出服务器时候,删除挂机缓存
 
     // 进入服务器
     @EventHandler
     public void event(PlayerJoinEvent event) {
         System.out.println(event.getPlayer().getDisplayName() + "进入了游戏!");
-        // 进入服务器时候,设置用户为活跃状态
         EhCacheUtil.setActive(event.getPlayer());
+    }
+
+    // ------------------------------------ test -----------------------------------------------------
+
+    // 玩家切换左右手事件
+    @EventHandler
+    public void event(PlayerSwapHandItemsEvent event) {
+        EhCacheUtil.setActive(event.getPlayer());
+        event.getPlayer().sendMessage(ChatColor.YELLOW + "切换为活跃状态");
     }
 
     // 玩家丢弃物品
