e47d9a2889513b433226cef7909e4c614576098d
tcyeee <tcyeee@outlook.com>
2022-01-13 20:50:05 +0800

    [develop] 版本更新

--- src/main/java/top/tcyeee/listener/PlayListener.java
+++ src/main/java/top/tcyeee/listener/PlayListener.java
@@ -1,9 +1,11 @@
 package top.tcyeee.listener;
 
+import com.Zrips.CMI.events.CMIAfkEnterEvent;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.*;
+import top.tcyeee.Main;
 import top.tcyeee.utils.EhCacheUtil;
 
 /**
@@ -13,12 +15,21 @@ import top.tcyeee.utils.EhCacheUtil;
  * @date 2021/12/23 16:36
  */
 public class PlayListener implements Listener {
+    public static Main main = Main.instance;
 
     // 玩家移动事件 => 设为活跃
     @EventHandler
     public void event(PlayerMoveEvent event) {
         Player player = event.getPlayer();
         EhCacheUtil.set(EhCacheUtil.status.active, player.getUniqueId().toString(), player);
+    }
+
+    // 玩家afk事件
+    @EventHandler
+    public void event(CMIAfkEnterEvent event) {
+        main.getLogger().warning("========================");
+        main.getLogger().warning("======= 开启AFK模式 =====");
+        main.getLogger().warning("========================");
     }
 
     // 进入服务器 => 设为活跃
