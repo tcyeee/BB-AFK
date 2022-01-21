3bb904552592ef5b8f0524c39c8a80b72cd5ab95
tcyeee <tcyeee@outlook.com>
2022-01-21 14:25:11 +0800

    [develop] 插件重命名

--- reset.sh
+++ reset.sh
@@ -1,8 +1,8 @@
 
 
 # 一键打包+替换,然后手动在服务器命令行界面输入reload即可完成代码更新
-rm -rf /Users/tcyeee/Documents/cy_project/minecraft/server/plugins/minecraft-0.1.jar
+rm -rf /Users/tcyeee/Documents/cy_project/minecraft/server/plugins/BenBenAfk-1.0.jar
 /Users/tcyeee/Documents/utils/apache-maven-3.6.3/bin/mvn package -f /Users/tcyeee/Documents/cy_project/minecraft/minecraft_benben/pom.xml
-mv /Users/tcyeee/Documents/cy_project/minecraft/minecraft_benben/target/minecraft-0.1.jar /Users/tcyeee/Documents/cy_project/minecraft/server/plugins/
+mv /Users/tcyeee/Documents/cy_project/minecraft/minecraft_benben/target/BenBenAfk-1.0.jar /Users/tcyeee/Documents/cy_project/minecraft/server/plugins/
 
 
